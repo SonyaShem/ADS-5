68ca3c884db45e03a9bb30e3e75aebe2af0f305d
SonyaShem <sonyasha2101@gmail.com>
2026-04-19 22:42:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,8 @@
 #include "tstack.h"
 
 int priority(char op) {
-    if(op=='+'||op=='-') return 1;
-    if(op=='*'||op=='/') return 2;
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
     return 0;
 }
 
