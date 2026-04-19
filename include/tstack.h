197ad1b575e8ea8f026be8adb224c81aed5a90a0
SonyaShem <sonyasha2101@gmail.com>
2026-04-19 22:47:21 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,25 +5,25 @@
 template<typename T, int size>
 class TStack {
     private:
-      T data[size];
-      int topIndex;
+        T data[size];
+        int topIndex;
     public:
-      TStack() {
-        topIndex = -1;
-      }
-      bool empty() {
-        return topIndex == -1;
-      }
-      void push(T value) {
-        topIndex++;
-        data[topIndex] = value;
-      }
-      void pop() {
-        topIndex--;
-      }
-      T top() {
-        return data[topIndex];
-      }
+        TStack() {
+            topIndex = -1;
+        }
+        bool empty() {
+            return topIndex == -1;
+        }
+        void push(T value) {
+            topIndex++;
+            data[topIndex] = value;
+        }
+        void pop() {
+            topIndex--;
+        }
+        T top() {
+            return data[topIndex];
+        }
 };
 
 #endif  // INCLUDE_TSTACK_H_
